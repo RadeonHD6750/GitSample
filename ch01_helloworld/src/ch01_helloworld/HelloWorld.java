d04fc5b2ded34b229c42c35914d6eed05dbddd28
jimin <thesuffering@naver.com>
2023-04-24 14:23:41 +0900

    first

---
+++ ch01_helloworld/src/ch01_helloworld/HelloWorld.java
@@ -0,0 +1,29 @@
+package ch01_helloworld;
+
+
+
+/******************************************************
+*
+* 클래스명 | HelloWorld
+* 	
+* 내용       | 개발환경 구축 확인용
+* 
+* 작성일    | 2023.04.06
+* 소속       | 개발1팀
+* 직급       | 주임
+* 성명       | 서지민
+
+https://github.com/RadeonHD6750/GitSample.git
+****************************************************/
+
+
+//Commit 테스트
+public class HelloWorld 
+{
+	
+	public static void main(String[] args)
+	{
+		System.out.println("Hello World");
+	}
+
+}
